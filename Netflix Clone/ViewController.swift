f6b9972386764c45eb25132e4386db144508009e
Ryan Adi Putra <ryannadiputraa@gmail.com>
2022-11-12 20:48:21 +0800

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Ryan Adi Putra on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
